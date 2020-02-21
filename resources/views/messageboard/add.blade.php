cbe06e360aa0c55eb0fdfd2dbfa6ff8ae485c22a
IGFeng <2218785142@qq.com>
2020-02-21 22:14:11 +0800

    一些改进

--- resources/views/messageboard/add.blade.php
+++ resources/views/messageboard/add.blade.php
@@ -11,7 +11,7 @@
         <p><img src=""><img src="" ></p><br>
         <label for="username">昵称:</label><input type="text" id="username" name="message[username]" value=""><br>
         <label for="content">内容:</label><textarea id="content" name="message[content]" cols="70" rows="9"></textarea> <br>
-        <label for="comment"></label><span>提交之前请先按CTRL+C保存您的留言内容，以免程序出错而丢失!留言内容最少五个字符</span><br>
+        <label for="comment"></label><span style="color:red">“我喝过很烈的酒，也放过不该放的手，从前不会回头，往后不会将就。”</span><br>
         <label for="ifqqh">悄悄话</label>
         <input name="message[ifqqh]" type="checkbox" id="ifqqh" value="1"><span>当选中时，此留言仅有管理员可见</span><br>
         <!--<label for="unum">验证码</label><input name="unum" type="text" id="message[unum]" size="10"><img src="randnum.php?id=-1" title="点击刷新" style="cursor:pointer" onclick=eval(this.src="randnum.php?id='+i+++'")><br>-->
